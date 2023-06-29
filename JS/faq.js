1ea2f7cbb97ae061d7df3ff84971058412dc1f8e
Iqboljon <iqboljonummataliyev770@gmail.com>
2023-06-29 20:53:56 +0500

    hello

---
+++ JS/faq.js
@@ -0,0 +1,35 @@
+const nightIcon = document.querySelector('.dark');
+const lightIcon = document.querySelector('.light');
+
+const header = document.getElementById('header');
+const headerLogoImg = document.querySelector('.header-logo-image');
+const Headermenu = document.querySelector('.menu');
+
+const nav = document.getElementById('nav');
+
+const body = document.getElementById('body');
+Headermenu.onclick = () =>{
+    nav.classList.toggle('show-hide-nav')
+}
+
+nightIcon.onclick = () =>{
+    nightIcon.classList.toggle('light-night');
+    lightIcon.classList.toggle('night-light');
+    header.classList.toggle('header-nav-night');
+    headerLogoImg.classList.toggle('header-logo-image2');
+
+    nav.classList.toggle('header-nav-night')
+   
+    body.classList.toggle('body-night');
+};
+
+lightIcon.onclick = () =>{
+    nightIcon.classList.toggle('light-night');
+    lightIcon.classList.toggle('night-light');
+    header.classList.toggle('header-nav-night');
+    headerLogoImg.classList.toggle('header-logo-image2');
+
+    nav.classList.toggle('header-nav-night')
+  
+    body.classList.toggle('body-night');
+}
