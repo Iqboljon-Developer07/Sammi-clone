905dc977aaa5534a694513f0de96854f0c021b82
Iqboljon <iqboljonummataliyev770@gmail.com>
2023-07-02 08:59:56 +0500

    hello

--- JS/contact.js
+++ JS/contact.js
@@ -9,6 +9,12 @@ const nav = document.getElementById('nav');
 
 const body = document.getElementById('body');
 const navigationContents = document.querySelectorAll('.navigation-content');
+
+const kontaktBox = document.querySelector('.kontakt-box');
+const kontaktRoyhatdanOtish = document.querySelector('.kontakt-royhatdan-otish');
+
+const kontaktHuquqlarHioyalangan = document.querySelector('.kontakt-huquqlar-hioyalangan');
+const huquqlarHioyalanganIcons = document.querySelectorAll('.huquqlar-hioyalangan-icons > div');
 navigationContents[3].classList.add('hello');
 Headermenu.onclick = () =>{
     nav.classList.toggle('show-hide-nav')
@@ -23,6 +29,13 @@ nightIcon.onclick = () =>{
     nav.classList.toggle('header-nav-night')
    
     body.classList.toggle('body-night');
+    kontaktBox.classList.toggle('color-white');
+    kontaktBox.classList.toggle('header-nav-night');
+    kontaktHuquqlarHioyalangan.classList.toggle('color-white')
+
+    // huquqlarHioyalanganIcons[0].classList.toggle('color-white')
+    // huquqlarHioyalanganIcons[1].classList.toggle('color-white')
+    // huquqlarHioyalanganIcons[2].classList.toggle('color-white')
 };
 
 lightIcon.onclick = () =>{
@@ -34,4 +47,11 @@ lightIcon.onclick = () =>{
     nav.classList.toggle('header-nav-night')
   
     body.classList.toggle('body-night');
+    kontaktBox.classList.toggle('color-white');
+    kontaktBox.classList.toggle('header-nav-night');
+    kontaktHuquqlarHioyalangan.classList.toggle('color-white')
+
+    // huquqlarHioyalanganIcons[0].classList.toggle('color-white')
+    // huquqlarHioyalanganIcons[1].classList.toggle('color-white')
+    // huquqlarHioyalanganIcons[2].classList.toggle('color-white')
 }
