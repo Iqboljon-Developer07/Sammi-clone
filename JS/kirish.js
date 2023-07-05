9f0a1e5efcce752f8161adfaf74f2616211a6499
Iqboljon <iqboljonwebdev@gmail.com>
2023-07-05 16:27:56 +0500

    hello

--- JS/kirish.js
+++ JS/kirish.js
@@ -59,3 +59,51 @@ form_swritcher2.onclick = () =>{
     signForm.classList.remove('yoq');
     signForm.classList.add('bor');
 }
+
+const form1 = document.querySelector('.sign-container-form');
+const form2 = document.querySelector('.sign-container-form2');
+
+const formInput1 = document.querySelector('.form-input1');
+const formInput2 = document.querySelector('.form-input2');
+
+const formWarningText = document.querySelectorAll('.warning')
+
+form1.addEventListener('submit', (i)=>{
+    i.preventDefault();
+    if (formInput1.value ==''){
+        formInput1.classList.add('form-input1-1');
+        formWarningText[0].classList.add('bor');
+    }
+    if (formInput2.value ==''){
+        formInput2.classList.add('form-input2-2');
+        formWarningText[1].classList.add('bor');
+    }
+});
+
+const form2Input1 = document.querySelector('.form2-input1');
+const form2Input2 = document.querySelector('.form2-input2');
+const form2Input3 = document.querySelector('.form2-input3');
+
+const form2WarningText = document.querySelectorAll('.form2-warning-text2-2')
+
+const containerText = document.querySelector('.container-texts');
+form2WarningText[0].classList.add('yoq');
+form2.addEventListener('submit',(i)=>{
+    i.preventDefault();
+    if(form2Input1.value == ''){
+        form2Input1.classList.add('form-input1-1');
+        form2WarningText[0].classList.add('bor');
+    }
+    if(form2Input2.value == ''){
+        form2Input2.classList.add('form-input1-1');
+        form2WarningText[1].classList.add('bor');
+        containerText.classList.add('bor')
+    }
+    if(form2Input3.value == ''){
+        form2Input3.classList.add('form-input1-1');
+        form2WarningText[2].classList.add('bor');
+        containerText.classList.add('bor')
+    }
+})
+
+console.log(form2WarningText);
